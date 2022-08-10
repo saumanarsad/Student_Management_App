e4a9369e04f8bf18693d3655e9a661237e04106f
Sauman Arshad <sauman.arshad@gmail.com>
2022-08-10 15:14:37 +0500

    Added third commit

--- Myapp/urls.py
+++ Myapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('add/', views.add,name='add'),
+    path('add/addrecord/', views.addrecord, name="addrecord"),
+
 ]
--- Myapp/views.py
+++ Myapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import Students
 from django.template import loader
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -10,3 +11,17 @@ def index(request):
     context = {"mystudents": mystudents}
     return HttpResponse(template.render(context,request))
 
+def add(request):
+    template = loader.get_template('add.html')
+    context = {}
+    return HttpResponse(template.render(context,request))
+
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    student = Students(firstname = x, lastname = y)
+    student.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def delete(request,id):
+    student = Students.objects.get(id=id)
